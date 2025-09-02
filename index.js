6dc827dca3d6e23e710cbbfa7cea6e553b4b32cb
Arush Choudhary <arushgzbin@gmail.com>
2025-09-02 22:43:15 +0530

    added shebang to avoid problems

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-const fs = require("fs")
-const path = require("path")
-const {execSync} = require("child_process")
+#!/usr/bin/env node
 
-const args = process.argv.slice(2)
-const projectName = args[0] || "my-app"
-const targetPath = path.join(process.cwd(), projectName)
+const fs = require("fs");
+const path = require("path");
+const { execSync } = require("child_process");
 
-const boilerplatePath = path.join(__dirname, "boilerplate")
+const args = process.argv.slice(2);
+const projectName = args[0] || "my-app";
+const targetPath = path.join(process.cwd(), projectName);
 
-fs.cpSync(boilerplatePath, targetPath, { recursive: true })
+const boilerplatePath = path.join(__dirname, "boilerplate");
 
-console.log(`✅ Project ${projectName} created`)
-console.log("\n\n\n\n")
+fs.cpSync(boilerplatePath, targetPath, { recursive: true });
+
+console.log(`✅ Project ${projectName} created`);
+console.log("\n\n\n\n");
 
 try {
-  execSync("git init", { stdio: "inherit" })
-  execSync("git add .", { stdio: "inherit" })
-  execSync('git commit -m "Initial commit -- basic server setup"', { stdio: "inherit" })
-  console.log("✅ Git repo initialized and first commit made")
+  execSync("git init", { stdio: "inherit" });
+  execSync("git add .", { stdio: "inherit" });
+  execSync('git commit -m "Initial commit -- basic server setup"', {
+    stdio: "inherit",
+  });
+  console.log("✅ Git repo initialized and first commit made");
 } catch (err) {
-  console.error("❌ Git initialization failed:", err.message)
+  console.error("❌ Git initialization failed:", err.message);
 }
 
-console.log(`cd ${projectName}`)
-console.log("npm install")
-console.log("npm run dev ")
-console.log("\n🚀 Happy coding!")
-
+console.log(`cd ${projectName}`);
+console.log("npm install");
+console.log("npm run dev ");
+console.log("\n🚀 Happy coding!");
